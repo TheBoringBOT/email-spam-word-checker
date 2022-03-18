05332e3a4f72df8754059a1684815885bcc15ea1
TheBoringBot <richardsgareth@icloud.com>
2022-03-18 16:47:21 +0100

    Started with text configuration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 
 import Layout from "./components/layouts/Layout";
+import useCheckSpam from "./hooks/useCheckSpam";
 
 function App() {
-  return <Layout />;
+  const [currentText, setCurrentText] = useState("");
+  const [currentTrue, setCurrentTrue] = useState("no");
+  // const spamWords = useCheckSpam();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { text } = e.target.elements;
+    setCurrentText(text.value);
+    console.table(Object.values(testWords));
+  };
+
+  return (
+    <Layout>
+      <h1 style={{ color: "red" }}>{currentTrue}</h1>
+      <h2>{currentText}</h2>
+      <form onSubmit={handleSubmit}>
+        <textarea rows="4" cols="50" id="text" />
+        <button type="submit">Evaluate</button>
+      </form>
+    </Layout>
+  );
 }
+
+const testWords = {
+  a: "dick",
+  b: "fish",
+};
 
 export default App;
