190980877d688e487b6b193bb6d7335235d6009a
TheBoringBot <richardsgareth@icloud.com>
2022-03-18 16:47:37 +0100

    added spam word list function

--- src/hooks/useCheckSpam.js
+++ src/hooks/useCheckSpam.js
@@ -1,6 +1,5 @@
-import React from "react";
 import SpamWordsList from "../SpamWordsList";
 
 export default function useCheckSpam() {
-  return console.log(SpamWordsList);
+  return SpamWordsList;
 }
