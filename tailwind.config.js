f167a490b1002362839a858f86a0a951ae6622a3
TheBoringBot <richardsgareth@icloud.com>
2022-03-20 12:52:20 +0100

    css improvements and assets added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bubble-gum": "#ff77e9",
+        accent: "#78dcca",
+      },
+    },
   },
   plugins: [],
 };
