72555477f9599c622e49c7474f4abd4084285cbb
TheBoringBot <richardsgareth@icloud.com>
2022-03-21 20:30:19 +0100

    centered text on mobile fix

--- src/components/layouts/parts/Footer.js
+++ src/components/layouts/parts/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="mt-auto flex flex-col md:flex-row justify-center items-center md:justify-between py-5 px-3 text-text-secondary">
-      <span className="text-sm">
+    <div className="mt-auto flex flex-col md:flex-row justify-center items-center text-center md:justify-between py-5 px-3 text-text-secondary">
+      <span className="text-sm mb-3 md:mb-0">
         “Either you run the day or the day runs you.” — Jim Rohn
       </span>
       <span className="text-sm">
