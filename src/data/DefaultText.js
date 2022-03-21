73a621307b9f11055c5b6e2aadc0bbe23a5d80ad
TheBoringBot <richardsgareth@icloud.com>
2022-03-21 17:02:18 +0100

    new file

---
+++ src/data/DefaultText.js
@@ -0,0 +1,14 @@
+const defaultText = `Dear friend,
+  
+I am a Financial Consultant in control of privately owned funds placed for long term investments.
+
+My client intends to invest these funds in projects. I am willing to finance projects at a guaranteed 5% ROI per annum for projects ranging from 2 years term and above but not exceeding 12 years.
+
+Please answer ASAP.
+
+Best regards,
+
+William S.
+`;
+
+export default defaultText;
